ca8c83fb2f9120057fc89186c2af2a24616349ae
f20250600-cmd <f20250600@pilani.bits-pilani.ac.in>
2026-08-29 16:45:46 +0000

    lab01 task2

--- labs/lab01/task2/tb.v
+++ labs/lab01/task2/tb.v
@@ -1,12 +1,7 @@
 // tb.v
-// Testbench for ripple_adder. This file is given -- do not modify it.
-// Time gaps are wide enough to let signals settle even with delays
-// present (used for both part (a) and part (b) of this task). The second
-// stimulus vector (7+1) deliberately makes the carry ripple through all
-// three internal carry wires (c1, c2, c3) -- and once delays are in the
-// picture, you can watch that ripple happen bit by bit in the waveform.
 
 module tb;
+
   reg  [3:0] t_a, t_b;
   reg        t_cin;
   wire [3:0] t_sum;
@@ -20,8 +15,8 @@ module tb;
     .cout (t_cout)
   );
 
-  // Waveform dump configuration
   string vcd_file;
+
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
@@ -31,14 +26,15 @@ module tb;
 
   initial begin
     t_a = 4'b0000; t_b = 4'b0000; t_cin = 0;
-    #20 t_a = 4'b0111; t_b = 4'b0001; t_cin = 0;   // carry ripples through c1,c2,c3
-    #20 t_a = 4'b1111; t_b = 4'b0001; t_cin = 0;   // sum=0000, cout=1
+    #20 t_a = 4'b0111; t_b = 4'b0001; t_cin = 0;
+    #20 t_a = 4'b1111; t_b = 4'b0001; t_cin = 0;
     #20 t_a = 4'b0101; t_b = 4'b0011; t_cin = 1;
     #20 t_a = 4'b1010; t_b = 4'b0101; t_cin = 0;
     #20 $finish;
   end
 
   initial
-    $monitor($time, " a=%b b=%b cin=%b | sum=%b cout=%b", t_a, t_b, t_cin, t_sum, t_cout);
+    $monitor($time, " a=%b b=%b cin=%b | sum=%b cout=%b",
+             t_a, t_b, t_cin, t_sum, t_cout);
 
 endmodule
