ff541e3cf60e0353b105a7fea1f9fc7e4e6d2f8e
f20250600-cmd <f20250600@pilani.bits-pilani.ac.in>
2026-08-29 16:43:14 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -1,32 +1,17 @@
-// FA_Gate.v
-
-// Gate-level model of a 1-bit full adder. No delays yet -- that starts in
-// Task 2. This task is purely about gate ordering.
-
 module FA_Gate(
-
   input a,
-
   input b,
-
   input cin,
-
   output sum,
-
   output cout
-
 );
 
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-
-  and (pc1, a,   b);
-
-  xor (sum, cin, ps);
-
-  and (pc2, cin, ps);
-
   or  (cout, pc1, pc2);
+  and (pc2, cin, ps);
+  xor (sum, cin, ps);
+  and (pc1, a, b);
+  xor (ps, a, b);
 
 endmodule
